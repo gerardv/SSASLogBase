caca4cd31f9b9f996704eddf5938b47fdefa105e
gerardv <gerard@veneman.net>
2019-11-09 21:30:43 +0100

    Add timezone toggle to index and details view.

--- SSASLogBase/wwwroot/js/site.js
+++ SSASLogBase/wwwroot/js/site.js
@@ -1,4 +1,20 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// When the user changes the timezone option...
+$("#TZSelector").change(function() {
+    var offset = 0;
+    //...to 'local'
+    if ($("#TZSelector option:selected").val() == "local") {
+        //get the user's utc offset using the moment.js library
+        offset = moment().utcOffset();
+    }
+
+    //and iterate over all time-class
+    $(".time").each(function() {
+        //get the original time from the aria-valuetext attribute
+        ct = moment($(this).attr('aria-valuetext'), "D/M/YYYY h:mm:ss A");
+        //add the user's utc offset
+        $(this).html(moment(ct).add(offset, 'm').format("D/M/YYYY h:mm:ss A"));
+    });
+});
